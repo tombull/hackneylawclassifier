517899c42ae9d0639241c30b5622d21d1adcd2f8
Tom Bull <tom@tombull.com>
2016-03-19 16:46:44 +0000

    arse

--- app/api_methods.py
+++ app/api_methods.py
@@ -20,7 +20,7 @@ import pytz
 import requests
 import json
 
-from hackney_law_data_client import *
+from app.hackney_law_data_client import *
 
 config = Configuration()
 config.host = HACKNEY_LAW_DATA_API_URL
